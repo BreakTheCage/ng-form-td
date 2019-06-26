96ce2c47f7866c1eca95d2614d2367449f34593c
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-06-26 09:46:05 +0400

    #hurry up 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,26 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   private defaultQuestion = "teacher";
   answer = '';
+  genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    //form.setValue: Whole form
+    //form.patchValue: Part of form
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ""
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
   }
   // onSubmit(form: NgForm){
   //   console.log(form)
