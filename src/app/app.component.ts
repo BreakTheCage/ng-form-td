70e5ef13e218a2edfc74163250b78e08be0c0d34
BreakTheCage <sarbaztanha707@outlook.com>
2019-06-26 15:51:40 +0400

    #Finished End 198; TD: Resetting Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
+  @ViewChild('f', {static: true}) signupForm: NgForm;
   private defaultQuestion = "teacher";
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
     //form.setValue: Whole form
@@ -36,5 +44,13 @@ export class AppComponent {
   // }
   onSubmit(){
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    
+    this.submitted = true;
+    this.signupForm.reset();
   }
 }
